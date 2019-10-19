0a18f4dd6a7a266e7efc2249fcdd42487a28fcc7
tarsilla <tarsillacosta.lima@gmail.com>
2019-10-19 15:16:58 -0300

    adicionando pacotes

---
+++ restProject/src/test/java/com/restproject/restProject/RestProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.restproject.restProject;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
